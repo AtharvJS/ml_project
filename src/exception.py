d096190fc940ba94ed3dec378571582c5c6ea285
AtharvJS <atharvyeram11@gmail.com>
2025-04-12 11:33:38 +0000

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging 
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -21,5 +21,5 @@ class CustomException(Exception):
 #     try:
 #         a = 1 / 0
 #     except ZeroDivisionError as e:
-#         logging.info('Divide by zero encountered')
+#         logging.info('Division by zero error encountered')
 #         raise CustomException(str(e), sys)
--- src/logger.py
+++ src/logger.py
@@ -30,3 +30,50 @@ logging.basicConfig(
 
 # if __name__ == '__main__':
 #   logging.info('Logging has satrted')
+
+# import logging
+# import os
+# from datetime import datetime
+# import pytz
+
+# class ISTFormatter(logging.Formatter):
+#     """
+#     Custom formatter to include IST timezone in the asctime.
+#     """
+#     def formatTime(self, record, datefmt=None):
+#         """
+#         Override formatTime to use IST.
+#         """
+#         ist_timezone = pytz.timezone('Asia/Kolkata')
+#         dt = datetime.fromtimestamp(record.created, tz=pytz.utc).astimezone(ist_timezone)
+#         if datefmt:
+#             s = dt.strftime(datefmt)
+#         else:
+#             s = dt.isoformat()  # Use isoformat for a detailed representation
+#         return s
+
+# # Get the IST timezone (defined only once)
+# indian_timezone = pytz.timezone('Asia/Kolkata')
+
+# # Get the current IST time
+# now_ist = datetime.now(indian_timezone)
+
+# log_file = now_ist.strftime('%d %B %Y at %I:%M %p') + ".log"
+# log_dir = os.path.join(os.getcwd(), 'logs')
+# os.makedirs(log_dir, exist_ok=True)
+# log_file_path = os.path.join(log_dir, log_file)
+
+# # Create a handler and set the formatter
+# handler = logging.FileHandler(log_file_path)
+# formatter = ISTFormatter(fmt='%(asctime)s - %(lineno)d %(name)s - %(levelname)s - %(message)s',
+#                            datefmt='%d %B %Y at %I:%M %p')  
+# handler.setFormatter(formatter)
+
+# # Create a logger and add the handler
+# # logger = logging.getLogger(__name__) # Or any name you want
+# # logger.addHandler(handler)
+# # logger.setLevel(logging.INFO)  # Set the logging level
+
+# # logger.info("This is a test log message in IST.")
+
+# # print(f"Log file created at: {log_file_path}")
